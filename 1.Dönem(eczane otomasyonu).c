aa87c3eb5a0c5067b782d594d4aae5afc614d28f
Kaan Akyüz <kaanaky77@gmail.com>
2021-02-21 00:32:03 +0300

    isim değiştirildi kodlar düzenlendi.

--- 1.Dönem(eczane otomasyonu).c
+++ 1.Dönem(eczane otomasyonu).c
@@ -1,56 +1,67 @@
-// 1.Sınıf 1.dönem Algoritma ve programlama
-
-#include<stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <time.h>
 #include <stdbool.h>
+#include <string.h>
 
 int DepoIlac(int n){
 	FILE *outfile,*infile;
-	char ch,ch1;
-	char IlacAdi[20],FirmaAdi[20];
-	int adet=0;
-	outfile=fopen("abc.txt","r");
 	
-	while(!feof(outfile)){
-		ch=getc(outfile);
-		printf("%c",ch);
+	if(n==0){
+		
+		char ch;
+		outfile=fopen("DepoilacListesi.txt","r");
+	
+		while(!feof(outfile)){
+			ch=getc(outfile);
+			printf("%c",ch);
+		}
+		fclose(outfile);
 	}
-	fclose(outfile);
+	
 	if(n==1){
-		printf("\n-------------\n"
+		char IlacAdi[30],FirmaAdi[30];
+		int adet=0;
+		float fiyat=0;
+		
+		printf("\n---------------\n"
 		"Ilac Adi giriniz:");
-		scanf("%s",IlacAdi);
-		//gets(IlacAdi);
+		fflush(stdin); //gets kullanmak sakıncalı olduğu için 
+		gets(IlacAdi);
+		
 		printf("Firma Adini giriniz:");
-		scanf("%s",FirmaAdi);
+		fflush(stdin);
+		gets(FirmaAdi);
+				
 		printf("Adet sayisini giriniz:");
 		scanf("%d",&adet);
-		infile=fopen("abc.txt","a");	
-	
-		fprintf(outfile,"%s-----%s-----%d\n",IlacAdi,FirmaAdi,adet);	
+		
+		printf("Fiyat giriniz:");
+		scanf("%f",&fiyat);
+				
+		infile=fopen("DepoIlacListesi.txt","a+");	
+		fprintf(infile,"%s --- %s --- %d --- %f\n",IlacAdi,FirmaAdi,adet,fiyat);	
 		fclose(infile);
 	}
-	
-
 }
 void PersonelListe(){
-	char* personelListe[3]={"Ozan Yilmaz","Furkan Cesur","Cem Ulun"};
 	
-	for(int i=0; i<3; i++ ){
-		printf("%s\n",personelListe[i]);
-	}	
+	char* personelListe[3][2]={{"Ozan Yilmaz","35"},{"Furkan Cesur","27"},{"Elif Ak","35"}};
+	for(int i=0; i<3; i++){
+		for(int j=0; j<1; j++){
+			printf("\t %d) Ad-Soyad: %s  Yas: %s \n",i+1,personelListe[i][j],personelListe[i][j+1]);
+		}
+	}
 }
 
 void KisiList(){
 	
-	int tcNo[10]={
-	57854123,85247963,
-	35789621,65478932,
-	48154891,89451232,
-	48621321,23215468,
-	32154987,12345678};
+	long long int tcNo[10]={
+	57854123448,85247963123,
+	35789621321,65478932451,
+	48154891632,89451232623,
+	48621321412,23215468853,
+	32154987162,12345678713};
 	
 	char *Kisiler[10][2]={
 	{"Cem Candan","18"},
@@ -67,41 +78,39 @@ void KisiList(){
 	
 	for(int i=0; i<10; i++){
 		for(int j=0; j<1; j++){
-			printf("\tAd ve Soyad=%s | Yas=%s | TcNo=%d\n",Kisiler[i][j],Kisiler[i][j+1],tcNo[i]);
+			printf("\tAd ve Soyad=%s | Yas=%s | TcNo=%lld\n",Kisiler[i][j],Kisiler[i][j+1],tcNo[i]);
 		}
 	}
 }
-
 void yuklemeEkrani(){
 		char *yuklemeEkrani[3]={"Veri Tabani yukleniyor","Sistem Guncelleniyor","Bilgiler Aliniyor"};
 		printf("----Ak Eczanesine Hosgeldiniz----\n"
 		"Sistem Yukleniyor.\nLutfen Bekleyiniz.\n");
-		//sleep(2); // bilerek yavaşlatma
+		sleep(1); // bilerek yavaşlatma
 		int sayac=0;
 		for(int i=0; i<3; i++){
 			printf("%s",yuklemeEkrani[i]);
 			while(sayac<=3){
 				printf("%c",46); // ascii tablosu  . (Nokta) simgesi
-				//sleep(1);
+				sleep(1);
 				sayac++;
 			}
 			printf("\n");
 			sayac=0;
-			//sleep(1);
+			sleep(1);
 		}
 }
 int main(void) {
 	
-	bool cikis=true;
+	bool cikis=true; //whileden istenildiği zaman çıkmak için
 	yuklemeEkrani();  // fonksiyon çağırma
 	do{
-		printf("\nYapmak istediklerinizi asagidaki listeden seciniz lutfen:\n"
+		printf("\n\n1Yapmak istediklerinizi asagidaki listeden seciniz lutfen:\n" // Kullanıcıya sunulan seçim listesi
 		"1)Depodaki ilac listesini getir.\n"
 		"2)Eczaneye kayitli hastalari getir.\n"
 		"3)Personel Listesini getir.\n"
 		"4)Depoya ilac ekle.\n"
-		"5)Eczaneye hasta ekle.\n"
-		"6)Cikis.\n");
+		"5)Cikis.\n");
 	int liste;
 	
 	scanf("%d",&liste);
@@ -119,9 +128,6 @@ int main(void) {
 			DepoIlac(1);
 			break;
 		case 5:
-			//HastaEkle();
-			break;
-		case 6:
 			cikis=false;
 	}
 	}while(cikis);
